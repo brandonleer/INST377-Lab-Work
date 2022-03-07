2e1abae317852b39812bab1fcdd8aaaf2b1ee628
Brandon Leer <bleer1281@gmail.com>
2022-03-06 20:37:29 -0500

    main

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -2,19 +2,10 @@ let slidePosition = 0;
 const slides = document.querySelectorAll('.carousel_item');
 const totalSlides = slides.length;
 
-document.querySelector('#carousel_button--next')
-  .addEventListener('click', () => {
-    moveToNextSlide();
-  });
-document.querySelector('#carousel_button--prev')
-  .addEventListener('click', () => {
-    moveToPrevSlide();
-  });
-
 function updateSlidePosition() {
-  for (const slide of slides) {
-    slides.classList.remove('carousel_item--visible');
-    slides.classList.add('carousel_item--hidden');
+  for (let slide of slides) {
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
   }
   slides[slidePosition].classList.add('carousel_item--visible');
 }
@@ -37,3 +28,12 @@ function moveToPrevSlide() {
   }
   updateSlidePosition();
 }
+
+document.querySelector('#carousel_button--next')
+  .addEventListener('click', () => {
+    moveToNextSlide();
+  });
+document.querySelector('#carousel_button--prev')
+  .addEventListener('click', () => {
+    moveToPrevSlide();
+  });
