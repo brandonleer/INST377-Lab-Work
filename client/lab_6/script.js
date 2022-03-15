70b008aed214f778e5df78de0a81b2d6e9f44b51
Brandon Leer <bleer1281@gmail.com>
2022-03-15 13:15:51 -0400

    main

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -16,7 +16,6 @@ async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form');
   const submit = document.querySelector('.submit_button');
   submit.style.display = 'none';
-  
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
   if (arrayFromJson.data.length > 0) {
