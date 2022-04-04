874b6652d38e75be6ed477a8bb9123bd89588299
Brandon Leer <bleer1281@gmail.com>
2022-04-04 10:18:03 -0400

    main

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -6,8 +6,8 @@ function getRandomIntInclusive(min, max) {
   );
 }
 function restoArrayMake(dataArray) {
-  console.log('fired dataHandler');
-  console.table(dataArray);
+  // console.log('fired dataHandler');
+  // console.table(dataArray);
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
     const restNum = getRandomIntInclusive(0, dataArray.length - 1);
@@ -21,8 +21,8 @@ function restoArrayMake(dataArray) {
 }
 
 function createHtmlList(collection) {
-  console.log('fired HTML creator');
-  console.log(collection);
+  // console.log('fired HTML creator');
+  // console.log(collection);
   const targetList = document.querySelector('.resto-list');
   targetList.innerHTML = '';
   collection.forEach((item) => {
@@ -35,16 +35,21 @@ function createHtmlList(collection) {
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form');
   const submit = document.querySelector('.submit_button');
+  const resto = document.querySelector('#resto_name');
+  const zipcode = document.querySelector('#zipcode');
   submit.style.display = 'none';
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-  console.log(arrayFromJson);
+  // console.log(arrayFromJson);
 
   if (arrayFromJson.data.length > 0) {
     submit.style.display = 'block';
+    resto.addEventListener('input', async (event) => {
+        console.log(event.target.value);
+    });
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
-      console.log('form submission'); // this is substituting for a "breakpoint"
+      // console.log('form submission'); // this is substituting for a "breakpoint"
       submit.style.display = 'block'; // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
       const restoArray = restoArrayMake(arrayFromJson.data);
