1c09a270cd7ffe8a3afb77908f83470608985032
Brandon Leer <bleer1281@gmail.com>
2022-04-03 23:45:50 -0400

    main

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,37 +1,37 @@
 function getRandomIntInclusive(min, max) {
   const newMin = Math.ceil(min);
   const newMax = Math.floor(max);
-  return Math.floor(Math.random() * (newMax - newMin + 1) + min);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
 }
 function dataHandler(dataArray) {
   console.log('fired dataHandler');
-  //console.table(dataArray);
+  // console.table(dataArray);
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
     const restNum = getRandomIntInclusive(0, dataArray.length - 1);
     return dataArray[restNum];
   });
-  //console.log(listItems);
+  // console.log(listItems);
   return listItems;
-//  range.forEach((item) => {
+  //  range.forEach((item) => {
   //  console.log('range item', item);
-  //});
-//}
+  // });
+  // }
 
-function restoArray(dataArray){
-
+  function createHtmlList(collection) {
+    console.log('fired HTML creator');
+    console.log(collection);
+    const targetList = document.querySelector('.resto-list');
+    targetList.innerHTML = '';
+    collection.forEach(((item) => {
+      const {name} = item;
+      const displayName = name.toLowerCase();
+      // eslint-disable-next-line no-template-curly-in-string
+      const injectThisItem = '<li>${displayName}</li>';
+      targetList.innerHTML += injectThisItem;
+    }));
+  }
 }
-function createHtmlList(collection){
-  console.log('fired HTML creator');
-  console.log(collection);
-  const targetList = document.querySelector('.resto-list')
-  targetList.innerHTML = "";
-  collection.forEach((item => {
-    const {name} = item;
-    const displayName = name.toLowerCase();
-    const injectThisItem = '<li>${displayName}</li>';
-    targetList.innerHTML += injectThisItem))};
-};
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form');
   const submit = document.querySelector('.submit_button');
@@ -45,7 +45,7 @@ async function mainEvent() { // the async keyword means we can make API requests
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       console.log('form submission'); // this is substituting for a "breakpoint"
-      /submit.style.display = 'block';// arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      submit.style.display = 'block'; // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
       const restoArray = dataHandler(arrayFromJson.data);
       createHtmlList(restoArray);
@@ -53,4 +53,4 @@ async function mainEvent() { // the async keyword means we can make API requests
   }
 }
 // this actually runs first! It's calling the function above
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); }// the async keyword means we can make API requests
+document.addEventListener('DOMContentLoaded', async () => mainEvent());
